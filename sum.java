36b3c12077aefecfea9c21c941cad9f1b60a9e9a
Bittu <kumarbittu2720@gmail.com>
2023-04-07 23:48:32 +0530

    newmulfeatures

--- sum.java
+++ sum.java
@@ -2,7 +2,10 @@ class A
 {
  public static void main(String[] args)
   {
+    int a=2;
+    int b=4;
     //int a=100;// variable declaration
       System.out.println("hello java how are u babu"); 
+    System.out.println("multiplication of the number"+a*b);
   }
 }
