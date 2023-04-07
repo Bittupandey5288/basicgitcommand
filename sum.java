39aa4a5f9ad9979d7c2af869f199f5187fca7196
Bittu <kumarbittu2720@gmail.com>
2023-04-07 07:53:40 +0530

    form is commited !

--- sum.java
+++ sum.java
@@ -3,6 +3,6 @@ class A
  public static void main(String[] args)
   {
     //int a=100;// variable declaration
-      System.out.println("hello java how are u"); 
+      System.out.println("hello java how are u babu"); 
   }
 }
