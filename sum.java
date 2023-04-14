a235d5009b206dfd738080a69cb4a9836e87fa3a
Bittu <kumarbittu2720@gmail.com>
2023-04-14 10:22:42 +0530

    hellog2

--- sum.java
+++ sum.java
@@ -2,10 +2,11 @@ class A
 {
  public static void main(String[] args)
   {
-    int a=2;
-    int b=4;
+    int a=4;
+    int b=2;
     //int a=100;// variable declaration
       System.out.println("hello java how are u babu"); 
-    System.out.println("multiplication of the number"+a*b);
+      System.out.println("multiplication of the number"+a*b);
+      System.out.println("division of the two number is ="+a/b);
   }
 }
